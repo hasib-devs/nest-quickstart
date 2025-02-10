51907a1c05e246716efc0c5e7d44360a68133af5
Hasibur Rahman <hasib.webdev@gmail.com>
2025-02-10 15:44:17 +0600

    Add environment variable for application port and update server startup log

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.APP_PORT ?? 3300;
+  console.log(`Server running on http://localhost:${port}`);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3300);
+  await app.listen(port);
 }
+
 bootstrap();
