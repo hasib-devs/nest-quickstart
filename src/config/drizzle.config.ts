ad8f6e0627e6b2b0621757d3fd53224792218ff1
Hasibur Rahman <hasib.webdev@gmail.com>
2025-02-11 03:07:13 +0600

    Update database migration files; replace schema paths and create new migration for blogs and users

--- src/config/drizzle.config.ts
+++ src/config/drizzle.config.ts
@@ -7,8 +7,8 @@ dotenv.config();
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/database/schemas',
-  out: './src/database/migrations',
+  schema: 'src/database/schemas',
+  out: 'src/database/migrations',
 
   dbCredentials: {
     url: DATABASE_URL,
