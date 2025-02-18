755d30524abd1e40809399df87c80e5829da29cb
Hasibur Rahman <hasib.webdev@gmail.com>
2025-02-18 11:21:51 +0600

    Refactor module imports: update import paths to use absolute paths and remove unused decorator file

--- src/common/modules/config.module.ts
+++ src/common/modules/config.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfig } from '@nestjs/config';
-import { validateEnv } from '../validators/env.validator';
+import { validateEnv } from '@/common/validators/env.validator';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 @Module({
   imports: [
--- src/common/utils/extract-jwt.ts
+++ src/common/utils/extract-jwt.ts
@@ -1,10 +1,6 @@
-export function extractToken(authorizationHeader: string): string | null {
+export function extractJwt(authorizationHeader: string): string | null {
   if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
     return null;
   }
   return authorizationHeader.replace('Bearer ', '');
-}
-
-export function extractJwt(authorizationHeader: string): string | null {
-  return extractToken(authorizationHeader);
 }
